6bc36059491ef5dd5118468106b591daf390146c
Avitoshsandve <129187663+Avitoshsandve@users.noreply.github.com>
2023-08-12 13:59:12 +0530

    Add files via upload

---
+++ anagramproblem.js
@@ -0,0 +1,16 @@
+//Write a function `isAnagram` which takes 2 parameters and returns true/false if those are anagrams or not.
+  //What's Anagram?
+  //- A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
+
+  //Once you've implemented the logic, test your code by running
+  function isanagram(str1,str2)
+  {
+    if(sort(str1)==sort(str2)){
+        return true;
+    }
+    else{
+        return false;
+    }
+  }
+  var ans=isanagram("kunju","junku");
+  console.log(ans)
